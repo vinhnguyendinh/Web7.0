819194fb28f0f2b88129bf5d602537b280c7b14f
Vinh Nguyen <vinhnguyendinhbg@gmail.com>
2017-08-01 20:31:21 +0700

    use bootstrap

--- TechkidsHotGirl/tkhotgirl/src/App.js
+++ TechkidsHotGirl/tkhotgirl/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Techkids Hot Girls
+        <Header />
       </div>
     );
   }
---
+++ TechkidsHotGirl/tkhotgirl/src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="container">
+          Techkids Hot Girl
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
